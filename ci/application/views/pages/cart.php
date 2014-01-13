43104a8d5703e1005976de22cb0981e5330364be
Jeanna <jawa@fullsail.edu>
2014-01-13 16:04:49 -0500

    added cart and fixed header
    added favicon

--- ci/application/views/pages/cart.php
+++ ci/application/views/pages/cart.php
@@ -2,7 +2,7 @@
 	<div class="box">
   		<div class="col-lg-12">
   			<hr>
-		    	<h2 class="intro-text text-center"> <strong>Cart</strong></h2>
+		    	<h2 class="intro-text text-center"><strong>Cart</strong></h2>
 		    <hr>
   			<div style="outline: 2px solid grey">
 		   		<ul>
@@ -10,6 +10,9 @@
 		   			<li>Something</li>
 		   		</ul>
 		    </div>
+		    <div class=" col-lg-5">
+              <a href="/index.php/signup" class="btn btn-default pull-right">Checkout</a>
+            </div>
   		</div>
 	</div>
 </div>
--- ci/application/views/template/header.php
+++ ci/application/views/template/header.php
@@ -5,6 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Photo Blog meets Etsy">
     <meta name="author" content="Jeanna Anderson">
+
+    <link rel="shortcut icon" href="<?php echo base_url(); ?>assets/img/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="<?php echo base_url(); ?>assets/img/favicon.ico" type="image/x-icon">
 	
 	<!-- Bootstrap core CSS -->
     <link href="<?php echo base_url(); ?>assets/css/bootstrap.css" rel="stylesheet">
@@ -14,7 +17,7 @@
 
     <!-- Add custom CSS here -->
     <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/stylesheet.css">
-	<!--<script src="//use.edgefonts.net/poiret-one.js"></script> -->
+	<script src="//use.edgefonts.net/poiret-one.js"></script> 
 	
 	</style>
 </head>
