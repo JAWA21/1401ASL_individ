bb7dcb5a71610cb0a58e014f1ccb20d1640207f3
Jeanna <jawa@fullsail.edu>
2014-01-13 15:48:35 -0500

    adding rest of the views

---
+++ ci/application/views/pages/cart.php
@@ -0,0 +1,15 @@
+<div class="row">
+	<div class="box">
+  		<div class="col-lg-12">
+  			<hr>
+		    	<h2 class="intro-text text-center"> <strong>Cart</strong></h2>
+		    <hr>
+  			<div style="outline: 2px solid grey">
+		   		<ul>
+		   			<li>Something</li>
+		   			<li>Something</li>
+		   		</ul>
+		    </div>
+  		</div>
+	</div>
+</div>
---
+++ ci/application/views/pages/login.php
@@ -0,0 +1,34 @@
+<div class="row">
+<div class="box">
+  <div class="col-lg-12">
+    <hr>
+    <h2 class="intro-text text-center"> <strong>Login</strong></h2>
+    <hr>
+        <form role="form">
+          <div class="row">
+            <div class="form-group col-lg-4">
+              <label>Username</label>
+              <input type="text" class="form-control">
+            </div>
+            <div class="form-group col-lg-4">
+              <label>Password</label>
+              <input type="email" class="form-control">
+            </div>
+            <div class="form-group col-lg-4">
+              <label for="input3">Confirm Password</label>
+              <input type="phone" class="form-control">
+            </div>
+            <div class=" col-lg-5">
+              <a href="/index.php/signup" class="pull-right">Register</a>
+            </div>
+         
+            <div class="form-group col-lg-12">
+              <input type="hidden" name="save" value="login">
+              <button type="submit" class="btn btn-default pull-right">Submit</button>
+            </div>
+            
+        </div>
+      </form>
+  </div>
+</div>
+</div>
